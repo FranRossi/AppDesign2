8d3f6445ad75b533a062bce334c56154b3a2e8f2
FranRossi <fr219401@fi365.ort.edu.uy>
2021-09-08 17:54:23 -0300

    Adds email property to User

--- BugSummary/Domain/User.cs
+++ BugSummary/Domain/User.cs
@@ -6,5 +6,6 @@
         public string lastName { get; set; }
         public string userName { get; set; }
         public string password { get; set; }
+        public string email { get; set; }
     }
 }
