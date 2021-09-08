1ad0290cbc67ef5b41fdeb9ad6f7e6610d375eff
FranRossi <fr219401@fi365.ort.edu.uy>
2021-09-08 17:15:29 -0300

    Create first test for class User, test's stage red

---
+++ BugSummary/Testing/UserTest.cs
@@ -0,0 +1,19 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testing
+{
+    [TestClass]
+    public class UserTest
+    {
+        [TestMethod]
+        public void CreateUserName()
+        {
+            User newUser = new User
+            {
+                firstName = "Juana"
+            };
+
+            Assert.AreEqual("Juana", newUser.firstName);
+        }
+    }
+}
