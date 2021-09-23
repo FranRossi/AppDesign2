681ab8e02fc05dc8dd829aabaab4f890fe7899ff
FranRossi <fr219401@fi365.ort.edu.uy>
2021-09-23 14:43:09 -0300

    Adds necessary parameters to DataAccess test file, stage red

--- BugSummary/DataAccessTest/UserRepositoryTest.cs
+++ BugSummary/DataAccessTest/UserRepositoryTest.cs
@@ -5,6 +5,32 @@ namespace DataAccessTest
     [TestClass]
     public class UserRepositoryTest
     {
+        private readonly DbConnection _connection;
+        private readonly UserRepository _userRepository;
+        private readonly BugSummaryContext _bugSummaryContext;
+        private readonly DbContextOptions<BugSummaryContext> _contextOptions;
+
+        public UserRepositoryTest()
+        {
+            this._connection = new SqliteConnection("Filename=:memory:");
+            this._contextOptions = new DbContextOptionsBuilder<BugSummaryContext>().UseSqlite(this._connection).Options;
+            this._userRepository = new BugSummaryContext(this._contextOptions);
+            this._userRepository = new UserRepository(this._bugSummaryContext);
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this._connection.Open();
+            this._bugSummaryContext.Database.EnsureCreated();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            this._bugSummaryContext.Database.EnsureDeleted();
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
