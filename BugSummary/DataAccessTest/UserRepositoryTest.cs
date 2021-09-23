552fca781fbdd76cc69295fa0499a1a5ab6025ad
FranRossi <fr219401@fi365.ort.edu.uy>
2021-09-23 14:38:30 -0300

    Creates DataAccessTest project

---
+++ BugSummary/DataAccessTest/UserRepositoryTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DataAccessTest
+{
+    [TestClass]
+    public class UserRepositoryTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
