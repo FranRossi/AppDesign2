64874bd3d0e91eeb9beeb111ab358de261b7e747
FranRossi <fr219401@fi365.ort.edu.uy>
2021-09-25 15:14:30 -0300

    Adds testclass to test ProjectRepository, stage red
    Adds only necessary parameters, to test repository

---
+++ BugSummary/DataAccessTest/ProjectRepositoryTest.cs
@@ -0,0 +1,45 @@
+﻿using DataAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Common;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+using Domain.DomainUtilities;
+using System.Collections.Generic;
+using System.Linq;
+using Utilities.Comparers;
+
+namespace DataAccessTest
+{
+
+    [TestClass]
+    public class ProjectRepositoryTest
+    {
+
+        private readonly DbConnection _connection;
+        private readonly ProjectRepository _projectRepository;
+        private readonly BugSummaryContext _bugSummaryContext;
+        private readonly DbContextOptions<BugSummaryContext> _contextOptions;
+
+        public ProjectRepositoryTest()
+        {
+            this._connection = new SqliteConnection("Filename=:memory:");
+            this._contextOptions = new DbContextOptionsBuilder<BugSummaryContext>().UseSqlite(this._connection).Options;
+            this._bugSummaryContext = new BugSummaryContext(this._contextOptions);
+            this._projectRepository = new ProjectRepository(this._bugSummaryContext);
+        }
+
+        [TestInitialize]
+        public void Setup()
+        {
+            this._connection.Open();
+            this._bugSummaryContext.Database.EnsureCreated();
+        }
+
+        [TestCleanup]
+        public void CleanUp()
+        {
+            this._bugSummaryContext.Database.EnsureDeleted();
+        }
+
+    }
