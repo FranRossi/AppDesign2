48a40916110e855eb1d95685ffa04ddd07bbb96d
FranRossi <fr219401@fi365.ort.edu.uy>
2021-09-25 10:14:59 -0300

    Makes necessary code to test bugrepository correctly

---
+++ BugSummary/DataAccessTest/BugRepositoryTest.cs
@@ -0,0 +1,33 @@
+﻿using DataAccess;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data.Common;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Domain;
+using Utilities;
+using System.Collections.Generic;
+using System.Linq;
+using DataAccess.Comparers;
+
+namespace DataAccessTest
+{
+    [TestClass]
+    public class BugRepositoryTest
+    {
+
+        private readonly DbConnection _connection;
+        private readonly BugRepository _bugRepository;
+        private readonly BugSummaryContext _bugSummaryContext;
+        private readonly DbContextOptions<BugSummaryContext> _contextOptions;
+
+        public BugRepositoryTest()
+        {
+            this._connection = new SqliteConnection("Filename=:memory:");
+            this._contextOptions = new DbContextOptionsBuilder<BugSummaryContext>().UseSqlite(this._connection).Options;
+            this._bugSummaryContext = new BugSummaryContext(this._contextOptions);
+            this._bugRepository = new UserRepository(this._bugSummaryContext);
+        }
+
+
+    }
+}
