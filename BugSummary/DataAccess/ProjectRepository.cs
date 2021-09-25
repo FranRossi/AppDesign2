233e6d5543f0db73a3d4b32bc1897a70a7b90e6a
FranRossi <fr219401@fi365.ort.edu.uy>
2021-09-25 15:15:58 -0300

    Adds constructor on ProjectRepository

---
+++ BugSummary/DataAccess/ProjectRepository.cs
@@ -0,0 +1,17 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+
+namespace DataAccess
+{
+    public class ProjectRepository
+    {
+        private readonly DbContext _context;
+
+        public ProjectRepository(BugSummaryContext bugSummaryContext)
+        {
+            this._context = bugSummaryContext;
+        }
+    }
+}
