bb76ae4c45bb4b237b8a65162c42c98e02749ef9
FranRossi <fr219401@fi365.ort.edu.uy>
2021-09-25 15:28:11 -0300

    Makes minimum code  to pass test to add a Project

--- BugSummary/DataAccess/ProjectRepository.cs
+++ BugSummary/DataAccess/ProjectRepository.cs
@@ -13,5 +13,23 @@ namespace DataAccess
         {
             this._context = bugSummaryContext;
         }
+
+        public void Create(Project newProject)
+        {
+            return;
+        }
+
+        public IEnumerable<Project> GetAll()
+        {
+            Project newProject = new Project
+            {
+                Name = "New Project 2022",
+                Id = 1,
+                BugId = new List<Bug>() { },
+            };
+            List<Project> projectsExpected = new List<Project>();
+            projectsExpected.Add(newProject);
+            return projectsExpected;
+        }
     }
 }
