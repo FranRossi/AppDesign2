cee1b2efbfa723ff8980cfc805f9bad6fd1a84a2
Agustin Ferrari Luciano <agustin_ferrari@hotmail.com>
2021-10-01 20:14:41 -0300

    Adds token property to User class

--- BugSummary/Domain/User.cs
+++ BugSummary/Domain/User.cs
@@ -11,5 +11,6 @@ namespace Domain
         public string Password { get; set; }
         public string Email { get; set; }
         public RoleType Role { get; set; }
+        public string Token { get; set; }
     }
 }
