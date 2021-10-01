043484a4b0ded4730fc0bfcd6ce5ef3e692f66b9
Agustin Ferrari Luciano <agustin_ferrari@hotmail.com>
2021-10-01 11:52:28 -0300

    Creates AddDeveloper test

--- BugSummary/BusinessLogicTest/UserLogicTest.cs
+++ BugSummary/BusinessLogicTest/UserLogicTest.cs
@@ -11,11 +11,10 @@ namespace BusinessLogicTest
     [TestClass]
     public class UserLogicTest
     {
-        [TestMethod]
+        [DataTestMethod]
         public void AddAdmin()
         {
             Mock<BugSummaryContext> _mockContext = new Mock<BugSummaryContext>(MockBehavior.Strict);
-
             User newUser = new User
             {
                 Id = 1,
@@ -25,6 +24,36 @@ namespace BusinessLogicTest
                 UserName = "pp",
                 Email = "pepe@gmail.com",
                 Role = RoleType.Admin
+            };
+            User receivedUser = null;
+            Mock<IRepository<User>> _mockUserRepository = new Mock<IRepository<User>>(MockBehavior.Strict);
+            _mockUserRepository.Setup(mr => mr.Add(It.IsAny<User>())).Callback((User newUser) =>
+            {
+                receivedUser = newUser;
+            });
+            _mockUserRepository.Setup(mr => mr.Save());
+
+
+            UserLogic userLogic = new UserLogic(_mockUserRepository.Object);
+            userLogic.Add(newUser);
+
+            _mockUserRepository.VerifyAll();
+            Assert.AreEqual(newUser, receivedUser);
+        }
+
+        [DataTestMethod]
+        public void AddDeveloper()
+        {
+            Mock<BugSummaryContext> _mockContext = new Mock<BugSummaryContext>(MockBehavior.Strict);
+            User newUser = new User
+            {
+                Id = 2,
+                FirstName = "Juan",
+                LastName = "Sanchez",
+                Password = "contra 123",
+                UserName = "juanito",
+                Email = "juan@gmail.com",
+                Role = RoleType.Developer
             };
             User receivedUser = null;
             Mock<IRepository<User>> _mockUserRepository = new Mock<IRepository<User>>(MockBehavior.Strict);
