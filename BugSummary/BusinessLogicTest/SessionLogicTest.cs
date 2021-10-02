264ff3348df94810cb9eed1ecca9be424f2c6711
Agustin Ferrari Luciano <agustin_ferrari@hotmail.com>
2021-10-02 15:01:29 -0300

    Refactors authenticate user test

--- BugSummary/BusinessLogicTest/SessionLogicTest.cs
+++ BugSummary/BusinessLogicTest/SessionLogicTest.cs
@@ -39,11 +39,20 @@ namespace BusinessLogicTest
             string username = "someUsername";
             string password = "somePassword";
             mockUserRepository.Setup(mr => mr.Authenticate(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+            string sentUsername = "";
+            string sentToken = "";
+            mockUserRepository.Setup(mr => mr.UpdateToken(It.IsAny<string>(), It.IsAny<string>()))
+                .Callback((string username, string token) =>
+                {
+                    sentUsername = username;
+                    sentToken = token;
+                });
 
             SessionLogic _sessionLogic = new SessionLogic(mockUserRepository.Object);
             string result = _sessionLogic.Authenticate(username, password);
 
             Assert.AreNotEqual(null, result);
+            mockUserRepository.Verify(mock => mock.UpdateToken(It.IsAny<string>(), It.IsAny<string>()), Times.Never());
         }
 
         [TestMethod]
@@ -53,12 +62,21 @@ namespace BusinessLogicTest
             string username = "someUsername";
             string password = "somePassword";
             mockUserRepository.Setup(mr => mr.Authenticate(It.IsAny<string>(), It.IsAny<string>())).Returns(false);
+            string sentUsername = "";
+            string sentToken = "";
+            mockUserRepository.Setup(mr => mr.UpdateToken(It.IsAny<string>(), It.IsAny<string>()))
+                .Callback((string username, string token) =>
+                    {
+                        sentUsername = username;
+                        sentToken = token;
+                    });
 
             SessionLogic _sessionLogic = new SessionLogic(mockUserRepository.Object);
             string result = _sessionLogic.Authenticate(username, password);
 
-
             Assert.AreEqual(null, result);
+            mockUserRepository.Verify(mock => mock.UpdateToken(It.IsAny<string>(), It.IsAny<string>()), Times.Once());
+            Assert.AreEqual(result, sentToken);
         }
     }
 }
