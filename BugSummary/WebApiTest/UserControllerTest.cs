a36f82cc718a0200f50fc13ccedd0dbde12d5c77
FranRossi <fr219401@fi365.ort.edu.uy>
2021-10-03 09:57:31 -0300

    Adds testCreate test for getting a user by token to get user by token
    stage red

--- BugSummary/WebApiTest/UserControllerTest.cs
+++ BugSummary/WebApiTest/UserControllerTest.cs
@@ -49,5 +49,42 @@ namespace WebApiTest
             ComparisonResult deepComparisonResult = compareLogic.Compare(expectedUser, receivedUser);
             Assert.IsTrue(deepComparisonResult.AreEqual);
         }
+        
+        [DataRow("estoEsUnTokenFake")]
+        [DataTestMethod]
+        public void GetUserByTokenTest(string token)
+        {
+            UserModel user = new UserModel
+            {
+                FirstName = "Pepe",
+                LastName = "Perez",
+                Password = "pepe1234",
+                UserName = "pp",
+                Email = "pepe@gmail.com",
+                Role = RoleType.Tester,
+            };
+            User expectedUser = new User
+            {
+                Id = 0,
+                FirstName = "Pepe",
+                LastName = "Perez",
+                Password = "pepe1234",
+                UserName = "pp",
+                Email = "pepe@gmail.com",
+                Role = RoleType.Tester,
+            };
+            Mock<ILogic<User>> mockUserLogic = new Mock<ILogic<User>>(MockBehavior.Strict);
+            mockUserLogic.Setup(m => m.Get(It.IsAny<string>())).Returns(user.ToEntity());
+            UserController controller = new UserController(mockUserLogic.Object);
+
+            
+            IActionResult result = controller.Get(token);
+
+            mockUserLogic.VerifyAll();
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+            CompareLogic compareLogic = new CompareLogic();
+            ComparisonResult deepComparisonResult = compareLogic.Compare(expectedUser, user.ToEntity());
+            Assert.IsTrue(deepComparisonResult.AreEqual);
+        }
     }
 }
