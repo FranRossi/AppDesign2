a356551a1063755bd0b9696b0945385122999123
Agustin Ferrari Luciano <agustin_ferrari@hotmail.com>
2021-10-03 18:55:44 -0300

    Adds AddValidProject in project controller

--- BugSummary/WebApiTest/ProjectControllerTest.cs
+++ BugSummary/WebApiTest/ProjectControllerTest.cs
@@ -31,5 +31,30 @@ namespace WebApiTest
             ComparisonResult deepComparisonResult = compareLogic.Compare(ExpectedProject, result);
             Assert.IsTrue(deepComparisonResult.AreEqual);
         }
+
+        [TestMethod]
+        public void AddValidProject()
+        {
+            ProjectModel projectToAdd = new ProjectModel
+            {
+                Name = "New Project 2022"
+            };
+            Project expectedProject = new Project
+            {
+                Name = "New Project 2022"
+            };
+            Mock<IProjectLogic> mock = new Mock<IProjectLogic>(MockBehavior.Strict);
+            Project receivedProject = null;
+            mock.Setup(m => m.Add(It.IsAny<Project>())).Callback((Project project) => receivedProject = project);
+            ProjectController controller = new ProjectController(mock.Object);
+
+            IActionResult result = controller.Post(user);
+
+            mock.VerifyAll();
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+            CompareLogic compareLogic = new CompareLogic();
+            ComparisonResult deepComparisonResult = compareLogic.Compare(expectedProject, receivedProject);
+            Assert.IsTrue(deepComparisonResult.AreEqual);
+        }
     }
 }
