9a559f0d169f86f50774702c8a382d37a78cedff
Agustin Ferrari Luciano <agustin_ferrari@hotmail.com>
2021-10-03 19:45:44 -0300

    Implements update on project repository and creates IProjectRepository

---
+++ BugSummary/DataAccessInterface/IProjectRepository.cs
@@ -0,0 +1,14 @@
+﻿using Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessInterface
+{
+    public interface IProjectRepository : IRepository<Project>
+    {
+        public void Update(Project updatedProject);
+    }
+}
