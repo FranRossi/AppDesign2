d51d78f8ef1e8d11cd75889b2c67f8b09387c77d
Agustin Ferrari Luciano <agustin_ferrari@hotmail.com>
2021-10-03 17:58:17 -0300

    Creates AddProject test on project logic tests

---
+++ BugSummary/BusinessLogicTest/ProjectLogicTest.cs
@@ -0,0 +1,40 @@
+﻿using BusinessLogic;
+using DataAccessInterface;
+using Domain;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicTest
+{
+    [TestClass]
+    class ProjectLogicTest
+    {
+        [TestMethod]
+        public void AddProject()
+        {
+            Project projectToAdd = new Project
+            {
+                Name = "New Project 2022"
+            };
+            Project receivedProject = null;
+            Mock<IRepository<Project>> mockUserRepository = new Mock<IRepository<Project>>(MockBehavior.Strict);
+            mockUserRepository.Setup(mr => mr.Add(It.IsAny<Project>())).Callback((Project newProject) =>
+            {
+                receivedProject = newProject;
+            });
+            mockUserRepository.Setup(mr => mr.Save());
+
+
+            ProjectLogic projectLogic = new ProjectLogic(mockUserRepository.Object);
+            projectLogic.Add(newUser);
+
+            mockUserRepository.VerifyAll();
+            Assert.AreEqual(projectToAdd, receivedProject);
+        }
+    }
+}
