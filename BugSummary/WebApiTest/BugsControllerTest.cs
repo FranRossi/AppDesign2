d0fe771a88c7656fc65144cac9a76572429f48e3
FranRossi <fr219401@fi365.ort.edu.uy>
2021-10-03 23:57:41 -0300

    Adds test to update bug on WebApiTest

--- BugSummary/WebApiTest/BugsControllerTest.cs
+++ BugSummary/WebApiTest/BugsControllerTest.cs
@@ -4,6 +4,7 @@ using BusinessLogicInterface;
 using DataAccessInterface;
 using Domain;
 using Domain.DomainUtilities;
+using KellermanSoftware.CompareNetObjects;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -47,6 +48,36 @@ namespace WebApiTest
             Assert.AreEqual(200,okResult.StatusCode);
             Assert.AreEqual(bugsExpected,bugsResult);
             CollectionAssert.AreEqual((ICollection) bugsExpected, (System.Collections.ICollection)bugsResult, new BugComparer());
+        }
+        
+        [DataRow("1pojjYCG2Uj8WMXBteJYRqqcJZIS3dNL")]
+        [DataTestMethod]
+        [TestMethod]
+        public void UpdateValiBug(string token)
+        {
+            BugModel bug = new BugModel
+            {
+                Name = "Bug2021",
+                Description = "ImportanteBug",
+                State = BugState.Active,
+                Version = "2",
+                ProjectId = 1,
+            };
+            Mock<IBugLogic> mock = new Mock<IBugLogic>(MockBehavior.Strict);
+            User receivedBug = null;
+            mock.Setup(m => m.Update(It.IsAny<string>(), It.IsAny<User>())).Callback((string token,User sentBug) =>
+            {
+                receivedBug = sentBug;
+            });
+            BugsController controller = new BugsController(mock.Object);
+
+            IActionResult result = controller.Post(bug);
+
+            mock.VerifyAll();
+            Assert.IsInstanceOfType(result, typeof(OkResult));
+            CompareLogic compareLogic = new CompareLogic();
+            ComparisonResult deepComparisonResult = compareLogic.Compare(bug.ToEntity(), receivedBug);
+            Assert.IsTrue(deepComparisonResult.AreEqual);
         }
     }
 }
