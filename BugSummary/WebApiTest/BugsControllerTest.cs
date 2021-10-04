b69648ee895c4dfd9f3f4012a1bc0b417aa431e2
FranRossi <fr219401@fi365.ort.edu.uy>
2021-10-04 14:05:43 -0300

    Adds test to check developer is not adding a bug

--- BugSummary/WebApiTest/BugsControllerTest.cs
+++ BugSummary/WebApiTest/BugsControllerTest.cs
@@ -47,6 +47,32 @@ namespace WebApiTest
             ComparisonResult deepComparisonResult = compareLogic.Compare(bug.ToEntity(), receivedBug);
             Assert.IsTrue(deepComparisonResult.AreEqual);
         }
+        
+        [DataRow("1pojjYCG2Uj8WMXBteJYRqqcJZIS3dNL")]
+        [DataTestMethod]
+        [TestMethod]
+        public void DeveloperAddsBug(string token)
+        {
+            BugModel bug = new BugModel
+            {
+                Name = "Bug2021",
+                Description = "ImportanteBug",
+                State = BugState.Active,
+                Version = "2",
+                ProjectId = 1,
+            };
+            Mock<IBugLogic> mock = new Mock<IBugLogic>(MockBehavior.Strict);
+            Bug receivedBug = null;
+            mock.Setup(m => m.Add(It.IsAny<string>(), It.IsAny<Bug>())).Throws(new UserMustBeTesterException());
+            BugsController controller = new BugsController(mock.Object);
+
+            TestExceptionUtils.Throws<UserMustBeTesterException>(
+                () => controller.Put(token,bug), "User's role must be tester for this action"
+            );
+
+        }
+        
+        
         [DataRow("1pojjYCG2Uj8WMXBteJYRqqcJZIS3dNL")]
         [DataTestMethod]
         public void GetBugsForTester(string token)
