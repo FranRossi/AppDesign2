8c743029493abbbe1d77a22ded6c62687fbaef3c
Agustin Ferrari Luciano <agustin_ferrari@hotmail.com>
2021-10-04 10:04:04 -0300

    Adds DissociateUserFromProject declaration to project repository interface

--- BugSummary/DataAccessInterface/IProjectRepository.cs
+++ BugSummary/DataAccessInterface/IProjectRepository.cs
@@ -14,5 +14,7 @@ namespace DataAccessInterface
         public void Delete(int projectId);
 
         public void AssignUserToProject(int projectId, int userId);
+
+        public void DissociateUserFromProject(int userId, int projectId);
     }
 }
