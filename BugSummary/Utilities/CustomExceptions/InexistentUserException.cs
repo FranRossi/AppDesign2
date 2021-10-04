906c2f07507d8a5298286137e54f049995be0d79
Agustin Ferrari Luciano <agustin_ferrari@hotmail.com>
2021-10-03 23:25:46 -0300

    Creates InexistentUserException and implements it on AssignUserToProject method

---
+++ BugSummary/Utilities/CustomExceptions/InexistentUserException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities.CustomExceptions
+{
+    public class InexistentUserException : Exception
+    {
+        public override string Message => "The entered user does not exist.";
+    }
+}
