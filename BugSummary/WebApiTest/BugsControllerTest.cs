0045898d753caf603fd4a504d0ea699b7c162d7c
Francisco Rossi <65981860+FranRossi@users.noreply.github.com>
2021-10-05 23:16:52 -0300

    Creates test for bugcontroller to create endpoint of bugs filtered

--- BugSummary/WebApiTest/BugsControllerTest.cs
+++ BugSummary/WebApiTest/BugsControllerTest.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Utilities.Comparers;
+using Utilities.Criterias;
 using WebApi.Controllers;
 using WebApi.Models;
 
@@ -135,6 +136,45 @@ namespace WebApiTest
             Assert.AreEqual(bugsExpected, bugsResult);
             CollectionAssert.AreEqual((ICollection)bugsExpected, (System.Collections.ICollection)bugsResult, new BugComparer());
         }
+        
+        [DataRow("1pojjYCG2Uj8WMXBteJYRqqcJZIS3dNL")]
+        [DataTestMethod]
+        public void GetBugsFilteredForUser(string token)
+        {
+            IEnumerable<Bug> bugsExpected = new List<Bug>()
+            {
+                new Bug()
+                {
+                    Id = 1,
+                    Name = "Bug2021",
+                    Description = "ImportanteBug",
+                    Project = new Project(),
+                    State = BugState.Active,
+                    Version = "2",
+                    ProjectId = 1,
+                }
+            };
+            BugSearchCriteria criteria = new BugSearchCriteria()
+            {
+                Name = "Bug1",
+                State = BugState.Active,
+                ProjectId = 1,
+                Id = 1
+            };
+            Mock<IBugLogic> mock = new Mock<IBugLogic>(MockBehavior.Strict);
+            mock.Setup(r => r.GetAll(It.IsAny<string>())).Returns(bugsExpected);
+            BugsController controller = new BugsController(mock.Object);
+
+            IActionResult result = controller.GetAllFiltered(token,criteria.MatchesCriteria);
+            OkObjectResult okResult = result as OkObjectResult;
+            IEnumerable<Bug> bugsResult = okResult.Value as IEnumerable<Bug>;
+
+            mock.VerifyAll();
+            Assert.AreEqual(200, okResult.StatusCode);
+            Assert.AreEqual(bugsExpected, bugsResult);
+            CollectionAssert.AreEqual((ICollection)bugsExpected, (System.Collections.ICollection)bugsResult, new BugComparer());
+        }
+        
 
         [DataRow("1pojjYCG2Uj8WMXBteJYRqqcJZIS3dNL")]
         [DataTestMethod]
