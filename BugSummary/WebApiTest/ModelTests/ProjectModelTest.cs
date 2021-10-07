da2e0f3fcf9512b05a801518d6408e722a076b04
Agustin Ferrari Luciano <agustin_ferrari@hotmail.com>
2021-10-07 01:06:26 -0300

    Moves project model tests to their own file

--- BugSummary/WebApiTest/ModelTests/ProjectModelTest.cs
+++ BugSummary/WebApiTest/ModelTests/ProjectModelTest.cs
@@ -21,6 +21,25 @@ namespace WebApiTest
     [TestClass]
     public class ProjectModelTest
     {
+
+        [TestMethod]
+        public void ProjectToEntityTest()
+        {
+            ProjectModel projectModel = new ProjectModel
+            {
+                Name = "New Project 2022"
+            };
+            Project ExpectedProject = new Project
+            {
+                Name = "New Project 2022"
+            };
+
+            Project result = projectModel.ToEntity();
+            CompareLogic compareLogic = new CompareLogic();
+            ComparisonResult deepComparisonResult = compareLogic.Compare(ExpectedProject, result);
+            Assert.IsTrue(deepComparisonResult.AreEqual);
+        }
+
         [TestMethod]
         public void InvalidModelToEntityNoId()
         {
@@ -30,6 +49,77 @@ namespace WebApiTest
             TestExceptionUtils.Throws<ProjectModelMissingFieldException>(
                 () => bugToCompare.ToEntity(), "Missing Fields: Required -> Name."
             );
+        }
+
+        public void ProjectBugCountToEntityTest()
+        {
+            IEnumerable<Project> projects = new List<Project>()
+            {
+                new Project
+                    {
+                        Name = "Project A",
+                        Bugs = new List<Bug> { new Bug(), new Bug(), new Bug(), }
+                    },
+                new Project
+                    {
+                        Name = "Project B",
+                        Bugs = new List<Bug> {  }
+                    },
+                new Project
+                    {
+                        Name = "Project C",
+                        Bugs = new List<Bug> { new Bug(), new Bug() }
+                    }
+            };
+            IEnumerable<ProjectBugCountModel> expectedModel = new List<ProjectBugCountModel>()
+            {
+                new ProjectBugCountModel
+                {
+                    Name = "Project A",
+                    BugCount = 3
+                },
+                new ProjectBugCountModel
+                {
+                        Name = "Project B",
+                        BugCount = 0
+                },
+                new ProjectBugCountModel
+                {
+                    Name = "Project C",
+                    BugCount = 2
+                }
+            };
+
+            IEnumerable<ProjectBugCountModel> model = ProjectBugCountModel.ToModel(projects);
+            CompareLogic compareLogic = new CompareLogic();
+            ComparisonResult deepComparisonResult = compareLogic.Compare(expectedModel, model);
+            Assert.IsTrue(deepComparisonResult.AreEqual);
+        }
+
+        [TestMethod]
+        public void ProjectZeroBugCountToEntityTest()
+        {
+            IEnumerable<Project> projects = new List<Project>()
+            {
+                new Project
+                    {
+                        Name = "Project A",
+                        Bugs = new List<Bug> { new Bug(), new Bug(), new Bug(), }
+                    }
+            };
+            IEnumerable<ProjectBugCountModel> expectedModel = new List<ProjectBugCountModel>()
+            {
+                new ProjectBugCountModel
+                {
+                    Name = "Project A",
+                    BugCount = 3
+                }
+            };
+
+            IEnumerable<ProjectBugCountModel> model = ProjectBugCountModel.ToModel(projects);
+            CompareLogic compareLogic = new CompareLogic();
+            ComparisonResult deepComparisonResult = compareLogic.Compare(expectedModel, model);
+            Assert.IsTrue(deepComparisonResult.AreEqual);
         }
     }
 }
