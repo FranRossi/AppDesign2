137a2d2d605ab3dacf5ecb93687163bf01bd46a5
FranRossi <fr219401@fi365.ort.edu.uy>
2021-10-27 16:59:25 -0300

    Adds code to validate properties and adds custom exception

--- BugSummary/Domain/Assignment.cs
+++ BugSummary/Domain/Assignment.cs
@@ -5,11 +5,47 @@ namespace Domain
 {
     public class Assignment
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public const int MaxAssignmentNameLength = 60;
+        public const int MaxAssignmentIdLength = 4;
+        
+        
+        private string _name;
+        private int _id;
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                ValidateIdLength(value);
+                _id = value;
+            }
+        }
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                ValidateName(value);
+                _name = value;
+            }
+        }
         public int HourlyRate { get; set; }
         public double Duration { get; set; }
         public Project Project { get; set; }
         public int ProjectId { get; set; }
+        
+        
+        private void ValidateName(string nameToValidate)
+        {
+            if (!Validator.MaxLengthOfString(nameToValidate, MaxAssignmentNameLength))
+                throw new AssignmentNameLengthIncorrectException();
+        }
+        
+        public void ValidateIdLength(int idToValidate)
+        {
+            if (!Validator.MaxLengthOfString(idToValidate.ToString(), MaxAssignmentIdLength))
+                throw new AssignmentIdLengthIncorrectException();
+        }
     }
 }
