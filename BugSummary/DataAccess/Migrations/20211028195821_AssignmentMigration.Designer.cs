f5880ad2d9ed2025df44e15738a4357ff756644d
FranRossi <fr219401@fi365.ort.edu.uy>
2021-10-28 17:22:52 -0300

    adds assignment migration to project
    Create table on data base

--- BugSummary/DataAccess/Migrations/20211028195821_AssignmentMigration.Designer.cs
+++ BugSummary/DataAccess/Migrations/20211028195821_AssignmentMigration.Designer.cs
@@ -4,14 +4,16 @@ using DataAccess;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DataAccess.Migrations
 {
     [DbContext(typeof(BugSummaryContext))]
-    partial class BugSummaryContextModelSnapshot : ModelSnapshot
+    [Migration("20211028195821_AssignmentMigration")]
+    partial class AssignmentMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
