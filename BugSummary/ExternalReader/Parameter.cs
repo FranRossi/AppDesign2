90e787f9def33eadff8f24fb190245fc4d549734
Agustin Ferrari Luciano <agustin_ferrari@hotmail.com>
2021-11-03 20:44:23 -0300

    Creates parameter class and parameter type enum

---
+++ BugSummary/ExternalReader/Parameter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalReader
+{
+    public class Parameter
+    {
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public ParameterType Type { get; set; }
+
+    }
+}
