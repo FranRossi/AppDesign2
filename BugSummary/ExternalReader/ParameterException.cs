56107624a80c4493d622c03760155e2db55a7efa
Agustin Ferrari Luciano <agustin_ferrari@hotmail.com>
2021-11-03 21:01:41 -0300

    Creates External reader's exceptions

---
+++ BugSummary/ExternalReader/ParameterException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalReader
+{
+    public class ParameterException : ExternalReaderException
+    {
+        public ParameterException()
+        {
+            messageToDisplay = "Invalid parameters, please check types and make sure not to leave any empty fields.";
+        }
+    }
+}
---
+++ BugSummary/ExternalReader/UnableToReadException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalReader
+{
+    public class UnableToReadException : ExternalReaderException
+    {
+        public UnableToReadException()
+        {
+            messageToDisplay = "Unable to read file, check path or read permissions.";
+        }
+    }
+}
