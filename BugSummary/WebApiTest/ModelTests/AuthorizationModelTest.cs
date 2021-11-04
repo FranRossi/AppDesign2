8fa048ce56806041d1a43c932e1ef38087f70d77
FranRossi <fr219401@fi365.ort.edu.uy>
2021-11-04 09:42:05 -0300

    Create test to convert token and role to model. Stage red

--- BugSummary/WebApiTest/ModelTests/AuthorizationModelTest.cs
+++ BugSummary/WebApiTest/ModelTests/AuthorizationModelTest.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics.CodeAnalysis;
 using Domain.DomainUtilities;
+using KellermanSoftware.CompareNetObjects;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using WebApi.Models;
 
@@ -19,6 +20,25 @@ namespace WebApiTest
             };
             Assert.IsNotNull(model);
         }
-
+        
+        [TestMethod]
+        public void BugToModel()
+        {
+            string token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpX";
+            RoleType role = RoleType.Admin;
+            AuthorizationModel expectedModel = new AuthorizationModel
+            {
+                Token = token,
+                Role = role
+            };
+            
+            AuthorizationModel newModel = AuthorizationModel.ToModel(token, role);
+            CompareLogic compareLogic = new CompareLogic();
+            ComparisonResult deepComparisonResult = compareLogic.Compare(expectedModel, newModel);
+            Assert.IsTrue(deepComparisonResult.AreEqual);
+        }
+        
     }
+    
+    
 }
