f78ede889eda1966a6c2ecf988d2d91b90719fcf
FranRossi <fr219401@fi365.ort.edu.uy>
2021-11-04 09:27:06 -0300

    Create Authorization model, to add role when returing token to frontEnd. Stage red

---
+++ BugSummary/WebApiTest/ModelTests/AuthorizationModelTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+[ExcludeFromCodeCoverage]
+public class AuthorizationModelTest
+{
+    [TestMethod]
+    public void CreateModel()
+    {
+        AuthorizationModel model = new AuthorizationModel
+        {
+           token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpX",
+           rol = 3
+        };
+        Assert.IsNotNull(model);
+    }
+    
+}
