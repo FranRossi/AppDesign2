57a6a7c953996e35bc967f45363d69fbd6e218df
Agustin Ferrari Luciano <agustin_ferrari@hotmail.com>
2021-11-03 21:18:53 -0300

    Implements External reader's bug model

---
+++ BugSummary/ExternalReader/BugModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalReader
+{
+    public class BugModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Version { get; set; }
+        public BugState State { get; set; }
+    }
+
+    public enum BugState
+    {
+        Fixed = 1,
+        Active = 2
+    }
+}
