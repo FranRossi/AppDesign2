489e727c44a8822b6cc457b138c67658868d20b6
Agustin Ferrari Luciano <agustin_ferrari@hotmail.com>
2021-11-06 20:55:52 -0300

    Creates IExternalReaderImporter and adds company 3 JSON reader

---
+++ BugSummary/ExternalReaderImporterInterface/IExternalReaderImporter.cs
@@ -0,0 +1,14 @@
+﻿using ExternalReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExternalReaderImporterInterface
+{
+    public interface IExternalReaderImporter
+    {
+        IEnumerable<Tuple<string, IEnumerable<Parameter>>> GetImportersInfo();
+    }
+}
