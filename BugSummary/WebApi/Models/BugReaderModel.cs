157b825cce16d37989f7d3a4951e0c6e42fef48b
Agustin Ferrari Luciano <agustin_ferrari@hotmail.com>
2021-11-07 14:42:30 -0300

    Implments bug reader model

---
+++ BugSummary/WebApi/Models/BugReaderModel.cs
@@ -0,0 +1,14 @@
+﻿using ExternalReader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.Models
+{
+    public class BugReaderModel
+    {
+        public string Path { get; set; }
+        public IEnumerable<Parameter> Parameters { get; set; }
+    }
+}
