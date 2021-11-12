9708bbca365d9d608fde2b307a83e0d793a8c5c9
FranRossi <fr219401@fi365.ort.edu.uy>
2021-11-12 18:21:36 -0300

    Adds bug service and a form to html

--- FrontEnd/argon-dashboard-angular-master/src/app/pages/bugs/bug/bug.component.ts
+++ FrontEnd/argon-dashboard-angular-master/src/app/pages/bugs/bug/bug.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {NgForm} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute} from '@angular/router';
+import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
+import {BugModel} from '../../../models/bugModel';
+import {BugService} from './bug.service';
 
 @Component({
   selector: 'app-bug',
@@ -7,9 +13,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BugComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('f') editBugForm: NgForm;
+  error = null;
+  bug: BugModel = null;
+  bugId: string;
+  isFetching = false;
+  constructor(private http: HttpClient, private bugService: BugService, private route: ActivatedRoute, private modalService: NgbModal) {
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.params.subscribe((params) => this.bugId = params['id']);
+    this.getBugById();
+  }
+
+  private getBugById() {
+    this.isFetching = true;
+    this.bugService.getBugById(this.bugId).subscribe({
+      next: (responseData) => {
+        this.isFetching = false;
+        this.bug = responseData;
+      },
+      error: (e) => {
+        this.isFetching = false;
+        this.error = e.status + ' ' + e.statusText;
+      }
+    });
+  }
+
+  onEditBug() {
+    const updatedBug: BugModel = this.editBugForm.value;
+    this.bugService.editBug(updatedBug, this.bug.id.toString())
+      .subscribe(responseData => {
+        this.bug = updatedBug;
+      });
+  }
+
+  onHandleError() {
+    this.error = null;
+  }
+
+  open(content, type, modalDimension) {
+    if (type === 'Notification') {
+      this.modalService.open(content, { windowClass: 'modal-danger', centered: true });
+    } else {
+      this.modalService.open(content, { centered: true });
+    }
   }
 
 }
