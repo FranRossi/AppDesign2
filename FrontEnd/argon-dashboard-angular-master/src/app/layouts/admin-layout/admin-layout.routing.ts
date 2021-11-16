de9740960dc5ffe726e21470314513443bb27a2d
FranRossi <fr219401@fi365.ort.edu.uy>
2021-11-16 16:26:37 -0300

    Redirects bug's button  on table to see bug's information

--- FrontEnd/argon-dashboard-angular-master/src/app/layouts/admin-layout/admin-layout.routing.ts
+++ FrontEnd/argon-dashboard-angular-master/src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -5,6 +5,7 @@ import { BugReadersComponent } from '../../pages/bug-readers/bug-readers.compone
 import { ProjectsComponent } from '../../pages/projects/projects.component';
 import {ProjectEditComponent} from '../../pages/projects/project/editProject.component';
 import { RegisterComponent } from '../../pages/register/register.component';
+import {BugViewerComponent} from '../../pages/bugs/bug-viewer/bug-viewer.component';
 
 export const AdminLayoutRoutes: Routes = [
     {
@@ -16,5 +17,7 @@ export const AdminLayoutRoutes: Routes = [
     { path: 'bug-editor-readers',   component: BugReadersComponent },
     { path: 'projects',         component: ProjectsComponent },
     { path: 'register',         component: RegisterComponent },
-    { path: 'projects/:id',         component: ProjectEditComponent}
+    { path: 'projects/:id',         component: ProjectEditComponent},
+    { path: 'bugs/:id',         component: BugViewerComponent},
+
 ];
